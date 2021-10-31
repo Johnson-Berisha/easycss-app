96c711f153a3a69bda2dab06b98381df7ee3d684
Johnson Berisha <gjonsonberisha@gmail.com>
2021-10-31 08:09:56 +0100

    Initial Commit #1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="ec-section">
+        <div className="home">
+          <div className="small-box">
+            <div className="content__small-box">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
